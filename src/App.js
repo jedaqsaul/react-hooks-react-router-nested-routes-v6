41eca4369361323450426c6e43c03ff4f9708b62
Aquila Jedidiah <jedaqsaul@gmail.com>
2025-04-20 23:37:35 +0300

    completed th react hooks react router nested routes v6 lab

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error(error));
+  }, []);
   return (
     <>
       <header>
-        
+        <NavBar />
       </header>
+      <Outlet context={users} />
     </>
   );
-};
+}
 
 export default App;
